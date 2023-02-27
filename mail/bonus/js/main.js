48849e8fd765751b7e0240bcd15e8021a4f914fa
Paolo Giovannini <giovannini13@gmail.com>
2023-02-27 15:47:05 +0100

    bonus concluso

---
+++ mail/bonus/js/main.js
@@ -0,0 +1,29 @@
+const emailDom = document.getElementById('email')
+
+const invioDom = document.getElementById('invio');
+
+const listaEmail =['pippo12@gmail.com', 'pluto12@gmail.com', 'paperino12@gmail.com', 'topolino12@gmail.com'];
+
+invioDom.addEventListener('click',
+
+    function() {
+        let controlloEmail = false;
+        let email = emailDom.value
+
+        for (let i = 0; i < listaEmail.length; i++) {
+    
+            if(email == listaEmail[i]){
+                controlloEmail = true;
+            }
+        }
+        
+        if(controlloEmail == false) {
+            document.querySelector(".accesso").innerHTML = `Non puoi entrare: la tua email è ${email} e non è presente nei nostri database`;
+            
+        } else{
+            document.querySelector(".accesso").innerHTML = `Puoi entrare: la tua email è ${email} ed è presente nei nostri database`;
+        }
+   
+    }
+
+)
